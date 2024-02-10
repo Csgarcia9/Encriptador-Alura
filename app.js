20fa5ae5828a3459ca38b31f0a460ddcdb9d94ba
Csgarcia9 <122550982+Csgarcia9@users.noreply.github.com>
2024-02-10 15:20:13 -0400

    Update app.js
    Empezando a crear las funciones.

---
+++ app.js
@@ -0,0 +1,16 @@
+function encriptarTexto(){
+
+
+
+
+    // Ocultar elemento por su ID
+
+    //Mostrar el boton de copiar el texto
+
+
+
+}
+
+function cambiarAttributoPorId(elemento, atributo){
+
+}
