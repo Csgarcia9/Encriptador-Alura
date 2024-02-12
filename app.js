7d58f93eb4bebe102bb0c0442c5b1fd6707813a6
Csgarcia9 <122550982+Csgarcia9@users.noreply.github.com>
2024-02-11 22:02:54 -0400

    Update app.js
    Ocultando los botones y demás elementos.

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-function encriptarTexto(){
+//Para acceder al boton por un click para remover y poner el hidden
+
+  let botonEncriptar = document.getElementById('encrypt-button');
+
+  botonEncriptar.addEventListener('click', ()=>{
+    
+    let elemento = document.getElementById('Copy-button');
+    elemento.removeAttribute('hidden');
+    let imagenMuneco = document.getElementById('imagen-muneco');
+    imagenMuneco.setAttribute('hidden',true);
+    let titleBox = document.getElementById('title-box');
+    titleBox.setAttribute('hidden',true);
+    let instructionMessage = document.getElementById('instruction-message');
+    instructionMessage.setAttribute('hidden',true);
 
 
-
-
-    // Ocultar elemento por su ID
-
-    //Mostrar el boton de copiar el texto
-
-
-
+    // Aqui agrega la funcion de encriptar el texto
+    const otraFuncion = () =>{
+    alert('Cambios agregados');
 }
-
-function cambiarAttributoPorId(elemento, atributo){
-
-}
+otraFuncion(); 
+});
