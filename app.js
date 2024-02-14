542efdfc200c46aaf03eb4eb87185b802c394d4c
Steven Garcia <stevengarciab09@outlook.com>
2024-02-14 10:37:44 -0400

    agregue el boton de copiado y puse en la logica de encriptar en cometarios para agregarla al boton cada una

--- app.js
+++ app.js
@@ -14,9 +14,60 @@
     instructionMessage.setAttribute('hidden',true);
 
 
-    // Aqui agrega la funcion de encriptar el texto
-    const otraFuncion = () =>{
-    alert('Cambios agregados');
-}
-otraFuncion(); 
+    // Boton de copiado del texto
+    
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const copyButton = document.getElementById('Copy-button');
+  const textoEncriptado = document.getElementById('text-result');
+
+  copyButton.addEventListener('click', () => {
+    const textoCopiado = textoEncriptado.textContent;
+    navigator.clipboard.writeText(textoCopiado)
+      .then(() => {
+        console.log("Texto copiado con éxito");
+      })
+      .catch(err => {
+        console.error("Error al copiar el texto", err);
+      });
+  });
+});
+
+
+/*Para la logica de encriptrar todo*/ 
+ 
+/*
+
+function reemplazarCaracteres(cadena){
+    const reemplazos = {'e':'enter','i':'imes','a':'ai','o':'ober','u':'ufat'};
+
+    //Aplicar el cambio de letras
+
+    let nuevaCadena = cadena.replace(/[aeiou]/g, function(match){
+        return reemplazos[match];
+    });
+    return nuevaCadena;
+}
+
+function volverInicio(cadena){
+    const reemplazos = {'enter':'e','imes':'i','ai':'a','ober':'o','ufat':'u'};
+
+    //Aplicar el cambio de letras
+
+    let cadenaOriginal = cadena.replace(/[enterimesaioberufat]/g, function(match){
+        return reemplazos[match];
+    });
+    return cadenaOriginal;
+}
+
+let cadenaOriginal = prompt('Digite una cadena, solo minusculas sin acentos:');
+let nuevaCadena = reemplazarCaracteres(cadenaOriginal);
+alert(`La cadena nueva es ${nuevaCadena}.`);
+
+cadenados = volverInicio(nuevaCadena);
+alert(`La cadena inicial es ${cadenados}.`);
+
+
+*/
